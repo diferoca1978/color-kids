6fb16a13e96269e6100dcac5b7249690326b1b3e
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2025-01-15 20:47:01 -0500

    estructuring the web page

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/preline/preline.js',
+	],
+	theme: {
+		colors: {
+			colorKidsBlue: '#89E7F1',
+			colorKidslightBlue: '#CBEEF3',
+			colorKidsfrenchRose: '#FB4D80',
+			colorKidsfrenchMauve: '#C461E1',
+			colorKidspearGreen: '#BFDE3A',
+			colorKidsorangeWeb: '#F3A41A',
+
+		},
+		extend: {},
+	},
+	plugins: [
+		require('preline/plugin'),
+	],
+}
