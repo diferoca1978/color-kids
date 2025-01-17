b3fc3be4671a3785d9578c7bd05cede23e75cb86
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2025-01-16 20:43:47 -0500

    Lenis smooth already works, footer have been added.

---
+++ src/scripts/lenisSmooth.js
@@ -0,0 +1,12 @@
+import Lenis from 'lenis'
+
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
